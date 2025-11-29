ebd74de31f8669466f76e056655f839029084dde
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-11-29 04:18:27 +0200

    Implementing `getComputerChoice` to get randome choice of the computer.

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * getComputerChoice() 
+ * get randome number between 0 and 100
+ * and for each range we get different choice each time
+ * @returns randome choice from list ["Rock", "Paper", "Scissors"]
+ */
+function getComputerChoice() {
+  let randomeNumber = Math.floor(Math.random() * 100);
+  let choice = "";
+
+  if (0 <= randomeNumber && randomeNumber <= 30) choice = "Rock";
+  else if (31 <= randomeNumber && randomeNumber <= 60) choice = "Paper";
+  else choice = "Scissors";
+  return choice;
+}
