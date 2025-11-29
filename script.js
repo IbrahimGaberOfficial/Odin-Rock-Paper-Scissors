9f5748dbc8347fb62d5c427cf71a47656d42f288
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-11-29 04:42:56 +0200

    Implementing `playRound( )` - it get humand and computer choises and return the winner or tie case.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * getComputerChoice() 
+ * getComputerChoice()
  * get randome number between 0 and 100
  * and for each range we get different choice each time
  * @returns randome choice from list ["Rock", "Paper", "Scissors"]
@@ -11,14 +11,39 @@ function getComputerChoice() {
   if (0 <= randomeNumber && randomeNumber <= 30) choice = "Rock";
   else if (31 <= randomeNumber && randomeNumber <= 60) choice = "Paper";
   else choice = "Scissors";
-  return choice;
+  console.log("computer choiced ", choice);
+  return choice.toLocaleLowerCase();
 }
 
 /**
  * Get the choice of user from prompt message
- */
-function getHumanChoice(){
-    let choice = prompt('Enter your choice from list \n [Rock, Paper, Scissors].')
+*/
+function getHumanChoice() {
+    let choice = prompt(
+        "Enter your choice from list \n [Rock, Paper, Scissors]."
+    );
     // console.log(typeof choice, choice);
-    return choice;
+    console.log("human choiced ", choice);
+    return choice.toLocaleLowerCase();
 }
+
+/**
+ * get humand and computer choices as parameters
+ * and check who is the winner.
+ */
+function playRound(humanChoice = "", computerChoice = "") {
+  // check for similar choices
+  if (humanChoice === computerChoice) return "it's a tie";
+  else if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  )
+    return "human";
+  else
+    return "computer";
+}
+let humandScore = 0,
+  computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice())
