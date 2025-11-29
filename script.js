49154232a541255ca595c17055d117341833ccb4
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-11-29 05:58:50 +0200

    Implementing `playGame( )` to play a whole game with 5 founds and view the winner at the end of the game.

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ function getComputerChoice() {
 
 /**
  * Get the choice of user from prompt message
-*/
+ */
 function getHumanChoice() {
-    let choice = prompt(
-        "Enter your choice from list \n [Rock, Paper, Scissors]."
-    );
-    // console.log(typeof choice, choice);
-    console.log("human choiced ", choice);
-    return choice.toLocaleLowerCase();
+  let choice = prompt(
+    "Enter your choice from list \n [Rock, Paper, Scissors]."
+  );
+  // console.log(typeof choice, choice);
+  console.log("human choiced ", choice);
+  return choice.toLocaleLowerCase();
 }
 
 /**
@@ -33,6 +33,7 @@ function getHumanChoice() {
  */
 function playRound(humanChoice = "", computerChoice = "") {
   // check for similar choices
+  alert(`you choiced ${humanChoice}, computer choiced ${computerChoice}`);
   if (humanChoice === computerChoice) return "it's a tie";
   else if (
     (humanChoice == "rock" && computerChoice == "scissors") ||
@@ -40,10 +41,36 @@ function playRound(humanChoice = "", computerChoice = "") {
     (humanChoice == "scissors" && computerChoice == "paper")
   )
     return "human";
-  else
-    return "computer";
+  else return "computer";
 }
+
+/**
+ * calls playRound to play 5 rounds,
+ * keeps track of the scores and declares a winner at the end.
+ */
+function playGame() {
+  let result;
+  for (let i = 1; i <= 5; i++) {
+    result = playRound(getHumanChoice(), getComputerChoice());
+    if (result == "human") {
+      alert("you win this round!.");
+      humandScore += 1;
+    } else if (result == "computer") {
+      alert("computer win this round!.");
+      computerScore += 1;
+    } else {
+      alert("It is a tie!.");
+    }
+  }
+  if (humandScore > computerScore) alert("you are the winner of the game!!.");
+  else alert("Computer is the winner of the game!!.");
+
+  humandScore = computerScore = 0;
+}
+
 let humandScore = 0,
   computerScore = 0;
 
-playRound(getHumanChoice(), getComputerChoice())
+const startBtn = document.getElementById("start-btn");
+// guessSubmit.addEventListener("click", checkGuess);
+startBtn.addEventListener("click", playGame);
